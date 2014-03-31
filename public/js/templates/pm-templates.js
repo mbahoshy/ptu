badf15717856741a64926503ed54080cfa05c4ac
mbahoshy <mbahoshy@gmail.com>
2014-03-31 10:20:26 -0700

    pm templates now rendering uniquely based on equipment type

--- public/js/templates/pm-templates.js
+++ public/js/templates/pm-templates.js
@@ -3,6 +3,28 @@ pm.templates.gasPM = "\
 	<div>Make: <input type='text' name='make' value='<%= make %>'></div>\
 	<div>Model: <input type='text' name='model' value='<%= model %>'></div>\
 	<div>Serial: <input type='text' name='serial' value='<%= serial %>'></div>\
+	<div>Inspect Heat Exchanger: <input type='checkbox' name='heatex' value=''></div>\
+	<div>Gas Pressures:\
+		<p>High: <input type='text' name='highgas' value=''></p>\
+		<p>Low: <input type='text' name='lowgas' value=''></p>\
+	</div>\
+	<input type='submit' value='Submit'>\
+	</form>\
+";
+
+
+pm.templates.electricPM = "\
+	<form>\
+	<div>Make: <input type='text' name='make' value='<%= make %>'></div>\
+	<div>Model: <input type='text' name='model' value='<%= model %>'></div>\
+	<div>Serial: <input type='text' name='serial' value='<%= serial %>'></div>\
+	<div>Inspect Blower Motor: <input type='checkbox' name='heatex' value=''></div>\
+	<div>Strips Heat:\
+		<p>#1: <input type='text' name='sh1' value=''></p>\
+		<p>#2: <input type='text' name='sh2' value=''></p>\
+		<p>#3: <input type='text' name='sh3' value=''></p>\
+		<p>#4: <input type='text' name='sh4' value=''></p>\
+	</div>\
 	<input type='submit' value='Submit'>\
 	</form>\
 ";
--- public/js/views/add-pm-view.js
+++ public/js/views/add-pm-view.js
@@ -2,9 +2,14 @@ pm.newPMView = Backbone.View.extend({
 	tagName: 'div',
     className: '',
     gas: _.template(pm.templates.gasPM),
+    electric: _.template(pm.templates.electricPM),
+    ac: _.template(pm.templates.electricPM),
+    heatpump: _.template(pm.templates.electricPM),
+    airhandler: _.template(pm.templates.electricPM),
+    split: _.template(pm.templates.electricPM),
     initialize: function (equipment) {
     	console.dir(equipment);
-		this.$el.append(this.gas(equipment));
+		this.$el.append(this[equipment.type](equipment));
 		$('#page').append(this.$el);
     }
 });
