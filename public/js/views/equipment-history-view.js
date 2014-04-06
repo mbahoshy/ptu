757d52ed17d2e9ac00b7c721d0ed41db609b7979
Matt <mbahoshy@gmail.com>
2014-04-06 12:34:47 -0700

    ticket view and part view now switching correctly

--- public/js/views/equipment-history-view.js
+++ public/js/views/equipment-history-view.js
@@ -2,22 +2,41 @@ pm.equipmentHistoryView = Backbone.View.extend ({
         tagName: 'div',
         id: 'equipment_history_container',
         template: _.template("Equipment History"),
+        optionstemplate:_.template("<div class='history-options'><span id='ticket_view'>Ticket View</span><span id='part_view'>Part View</span></div>"),
+        containertemplate: _.template("<div id='#ticket_container'></div>"),
+        events: {
+        	'click #part_view': 'partView',
+        	'click #ticket_view': 'ticketView'
+        },
         initialize: function (tickets) {
-			this.$el.html( this.template);
-			tickets.forEach(this.createView, this);
+			this.$el.append( this.template);
+			this.$el.append( this.optionstemplate);
+			this.$el.append( this.containertemplate);
+			this.tickets = tickets;
+			this.ticketView();
+    		       
 
-    		$('#page').append(this.$el);       
         },
         createView: function (model) {
         	var eh = new pm.equipmentHistoryPM (model);
         	this.$el.append(eh.$el);
+        },
+        ticketView: function () {
+			this.tickets.forEach(this.createView, this);
+			$('#page').append(this.$el);
+			// $('#ticket_container').append(this.$el);
+        },
+        partView: function () {
+        	console.dir(this.tickets);
+        	$('.equipment-history-item').remove();
         }
 
 });
 
+
 pm.equipmentHistoryPM = Backbone.View.extend ({
         tagName: 'div',
-        class: 'equipment-history-item',
+        className: 'equipment-history-item',
         gas: _.template(pm.templates.gasPMView),
 	    electric: _.template(pm.templates.electricPMView),
 	    ac: _.template(pm.templates.acPMView),
