df56cb8ebd558581ee95a797fb6344afb14b3b4b
mbahoshy <mbahoshy@gmail.com>
2014-04-06 20:56:57 -0700

    google graphs now rendering:

--- public/js/views/equipment-history-view.js
+++ public/js/views/equipment-history-view.js
@@ -27,6 +27,7 @@ pm.equipmentHistoryView = Backbone.View.extend ({
         partView: function () {
         	console.dir(this.tickets);
         	$('.equipment-history-item').remove();
+            var parthistory1 = new pm.equipmentPartHistory (this.tickets);
         }
 
 });
@@ -43,7 +44,61 @@ pm.equipmentHistoryPM = Backbone.View.extend ({
    		split: _.template(pm.templates.splitPMView),
         initialize: function (ticket) {
 			this.$el.append(this[ticket.type](ticket));
-			
+        }
+});
 
+pm.equipmentPartHistory = Backbone.View.extend ({
+        tagName: 'div',
+        className: 'equipment-history-item',
+        template: _.template('<div id="chart_div"></div>'),
+        initialize: function (tickets) {
+            // var dates = _.pluck(tickets, 'date');
+            var dates = _.map(tickets, function (data) {
+                var lowgas,
+                    highgas;
+                if (data.lowgas == '') {
+                    lowgas=0;
+                } else {
+                    lowgas = parseFloat(data.lowgas);
+                }
+
+                if (data.highgas == '') {
+                    highgas=0;
+                } else {
+                    highgas = parseFloat(data.highgas);
+                }
+
+                return [new Date(data.date), lowgas, highgas];
+            });
+
+        
+            this.$el.append(this.template);
+            $('#page').append(this.$el);
+            this.drawGraph(dates);
+            // console.dir(dates[0][0].typeof());
+        },
+        drawGraph: function (chartdata) {
+              // google.setOnLoadCallback(drawChart);
+              drawChart(chartdata);
+              function drawChart(chartdata) {
+                var dataTable = new google.visualization.DataTable();
+                
+                dataTable.addColumn('date', 'Date');
+                dataTable.addColumn('number', 'lowgas');
+                dataTable.addColumn('number', 'highgas');
+                dataTable.addRows(chartdata);
+                console.dir(chartdata);
+                
+                var dataView = new google.visualization.DataView(dataTable);
+                dataView.setColumns([{calc: function(data, row) { return data.getFormattedValue(row, 0); }, type:'string'}, 1]);
+
+                var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+                var options = {
+                    legend: 'none',
+                    pointSize: 5
+                };
+                chart.draw(dataView, options);
+
+              }
         }
 });
